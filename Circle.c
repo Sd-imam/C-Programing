43287eddfa0de685e92b60430ae64f539a7f9d74
SD Imam Shimul <sdimamshimul@gmail.com>
2025-03-17 00:33:20 +0600

    Circle

---
+++ Circle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    float radius,area;
+
+    printf("Enter radius: ");
+    scanf("%f",&radius);
+
+    area= 3.1416*radius*radius;
+
+    printf("The area is: %f\n",area);
+
+
+
+
+
+    getch();
+}
