da3764b01e26d69d2932361b5f8215f499fc1269
SD Imam Shimul <sdimamshimul@gmail.com>
2025-03-25 23:05:36 +0600

    Add files via upload

---
+++ 25 3 25 - 6 - FabuNacue.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int first=0,secound=1,fabu,num,cound=0;
+
+    printf("Enter any last number: ");
+    scanf("%d",&num);
+
+
+while(cound<num)
+{
+    if(cound<=1)
+        fabu=count;
+    else
+
+
+
+
+}
+
+
+
+
+
+
+    getch();
+}
