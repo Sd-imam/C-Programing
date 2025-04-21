f4fda383ba3b15c2c11752a21a0d0b367f89a89c
SD Imam Shimul <sdimamshimul@gmail.com>
2025-04-21 23:56:10 +0600

    Add files via upload

---
+++ 25 4 21 - String - upper.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[]="SD Imam Shimul";
+
+    strlwr(str);
+    printf("String : %s\n",str);
+    strupr(str);
+    printf("Sting  : %s\n",str);
+
+
+
+
+
+    getch();
+}
