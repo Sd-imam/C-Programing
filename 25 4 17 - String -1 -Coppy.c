546d091f88cd0df6742cf0ad35021ce18251e90a
SD Imam Shimul <sdimamshimul@gmail.com>
2025-04-21 23:57:58 +0600

    Add files via upload

---
+++ 25 4 17 - String -1 -Coppy.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+
+{
+    char sourcr[]="SD Imam Shimul";
+    char terget[30];
+
+    strcpy(terget,sourcr);
+
+    printf("Source string =%s\n",sourcr);
+    printf("Target String = %s\n",terget);
+
+
+
+    getch();
+}
