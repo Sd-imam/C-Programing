8ead023d4235cdeb538060fb73c3b69c5d0cfc82
SD Imam Shimul <sdimamshimul@gmail.com>
2025-04-21 23:57:43 +0600

    Add files via upload

---
+++ 25 4 17 - String - 2- StringCat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char str1[]="My name is  ";
+    char str2[]=" SD Imam Shimul";
+
+    strcat(str1,str2);
+
+    printf("String plus : %s\n",str1);
+
+
+
+    getch();
+}
